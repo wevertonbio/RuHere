6102c3ea159cb4060541b4f7bebf3522fa1c7798
wevertonbio <wevertonf1993@hotmail.com>
2025-11-04 12:40:18 -0300

    Remove example in the end of the function

--- R/fishbase_here.R
+++ R/fishbase_here.R
@@ -1,7 +1,7 @@
-fishbase_here <- function(data_dir, 
+fishbase_here <- function(data_dir,
                           data_version = "latest",
                           verbose = TRUE) {
-  
+
 
   if (is.null(data_dir)) {
     stop("data_dir should be specified")
@@ -21,37 +21,37 @@ fishbase_here <- function(data_dir,
   if (!inherits(verbose, "logical")) {
     stop("verbose should be logical")
   }
-  
+
   if (verbose) message("Downloading complete species list...")
   sp_list <- rfishbase::species(version = data_version)
   names_sp <- rfishbase::species_names()
   comp_list <- sp_list %>%
     dplyr::left_join(names_sp, by = "SpecCode")
 
-  data.table::fwrite(comp_list, 
+  data.table::fwrite(comp_list,
                      file.path(data_dir, "fishbase/fb_species_list.gz"))
-  
+
   if (verbose) message("Downloading country distribution data (C_Code)...")
-  suppressMessages(fb_country <- rfishbase::country(comp_list$Species, 
+  suppressMessages(fb_country <- rfishbase::country(comp_list$Species,
                                             version = data_version))
-  data.table::fwrite(fb_country, 
+  data.table::fwrite(fb_country,
                      file.path(data_dir, "fishbase/fb_species_country.gz"))
-  
-  if (verbose) message("Downloading FAO Area distribution data...")  
-  suppressMessages(fb_fao <- rfishbase::faoareas(comp_list$Species, 
+
+  if (verbose) message("Downloading FAO Area distribution data...")
+  suppressMessages(fb_fao <- rfishbase::faoareas(comp_list$Species,
                                          version = data_version))
   data.table::fwrite(fb_fao, file.path(data_dir, "fishbase/fb_species_fao.gz"))
-  
+
   if (verbose) message("Downloading ecosystem distribution data...")
-  suppressMessages(fb_ecosystem <- rfishbase::ecosystem(comp_list$Species, 
+  suppressMessages(fb_ecosystem <- rfishbase::ecosystem(comp_list$Species,
                                                 version = data_version))
   data.table::fwrite(fb_ecosystem, file.path(data_dir, "fishbase/fb_species_ecosystem.gz"))
 
   if (verbose) message("Downloading country decoder map...")
   suppressMessages(fb_decoder <- rfishbase::c_code(version = data_version))
-  data.table::fwrite(fb_decoder, 
+  data.table::fwrite(fb_decoder,
                      file.path(data_dir, "fishbase/fb_countries_decoder.gz"))
 }
 
-data_dir = "~/Documents/lab/RuHere/"
-fishbase_here(data_dir)
+# data_dir = "~/Documents/lab/RuHere/"
+# fishbase_here(data_dir)
