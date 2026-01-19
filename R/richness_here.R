cac491fe15615d255ab0f83f600e01d321863ea9
wevertonbio <wevertonf1993@hotmail.com>
2026-01-19 14:43:38 -0300

    Add crs argument and change mask in example

--- R/richness_here.R
+++ R/richness_here.R
@@ -18,9 +18,11 @@
 #' the output will match its resolution, extent, and CRS. Default is `NULL`.
 #' @param res (numeric) the desired resolution (in decimal degrees if WGS84)
 #' for the output grid. Only used if `raster_base` is `NULL`.
+#' @param crs (character) the coordinate reference system of the raster.
+#' (see ?terra::crs). Default is "epsg:4326". Only applicable if `raster_base`
+#' is not provided.
 #' @param mask (SpatRaster or SpatVector) an optional layer to mask the
-#' final output. If the CRS differs from the output, it will be
-#' automatically reprojected. Default is `NULL`.
+#' final output. Default is `NULL`.
 #' @param summary (character) the type of summary to calculate.
 #' Either `"records"` (number of occurrences per cell) or `"species"`
 #' (number of unique species per cell). Default is `"records"`.
@@ -37,7 +39,6 @@
 #'
 #' @importFrom terra rast vect project crs ext rasterize geom cellFromXY xyFromCell mask
 #' @importFrom maps map
-#' @importFrom sf st_as_sf
 #'
 #' @export
 #'
@@ -48,63 +49,79 @@
 #'
 #' # Record density map
 #' r_records <- richness_here(occ, res = 0.5, summary = "records")
+#' terra::plot(r_records)
 #'
 #' # Species richness map masked by Brazil's border
-#' brazil_sf <- sf::st_as_sf(maps::map("world", "Brazil", plot = FALSE, fill = TRUE))
-#' brazil_mask <- terra::vect(brazil_sf)
-#' r_richness <- richness_here(occ, res = 1, mask = brazil_mask, summary = "species")
+#' world <- terra::unwrap(RuHere::world) # Import world map
+#' brazil <- world[world$name == "brazil",] # Subset Brazil
+#' r_richness <- richness_here(occ, res = 1, mask = brazil, summary = "species")
+#' terra::plot(r_richness)
 #'
 #' # Average trait value per cell
 #' sim_mass <- c(runif(length(unique(occ$species)), 10, 20))
 #' names(sim_mass) <- unique(occ$species)
 #'
-#' r_trait <- richness_here(occ, res = 0.5, summary = "species", field = sim_mass, fun = mean)
+#' r_trait <- richness_here(occ, res = 0.5, summary = "species", mask = brazil,
+#'                          field = sim_mass, fun = mean)
+#' terra::plot(r_trait)
 #'
 richness_here <- function(occ, species = "species", long = "decimalLongitude",
                           lat = "decimalLatitude", raster_base = NULL,
-                          res = NULL, mask = NULL, summary = "records",
+                          res = NULL, crs = "epsg:4326",
+                          mask = NULL, summary = "records",
                           field = NULL, fun = mean, verbose = TRUE) {
 
   if (!inherits(occ, "data.frame")) stop("`occ` must be a data.frame.", call. = FALSE)
-  if (!is.null(species) && !inherits(species, "character")) stop("`species` must be a character vector.", call. = FALSE)
-  if (!inherits(long, "character")) stop("`long` must be a character vector.", call. = FALSE)
-  if (!inherits(lat, "character")) stop("`lat` must be a character vector.", call. = FALSE)
-  if (!inherits(summary, "character") || !summary %in% c("records", "species")) stop("`summary` must be either 'records' or 'species'.", call. = FALSE)
-  if (!inherits(fun, "function")) stop("`fun` must be a function.", call. = FALSE)
-  if (!is.null(res) && !inherits(res, c("numeric", "integer"))) stop("`res` must be a numeric value.", call. = FALSE)
-  if (!is.null(raster_base) && !inherits(raster_base, "SpatRaster")) stop("`raster_base` must be a SpatRaster object.", call. = FALSE)
-  if (!is.null(mask) && !inherits(mask, c("SpatRaster", "SpatVector"))) stop("`mask` must be a SpatRaster or SpatVector.", call. = FALSE)
+  if (!is.null(species) && !inherits(species, "character"))
+    stop("`species` must be a character vector.", call. = FALSE)
+  if (!inherits(long, "character"))
+    stop("`long` must be a character vector.", call. = FALSE)
+  if (!inherits(lat, "character"))
+    stop("`lat` must be a character vector.", call. = FALSE)
+  if (!inherits(summary, "character") || !summary %in% c("records", "species"))
+    stop("`summary` must be either 'records' or 'species'.", call. = FALSE)
+  if (!inherits(fun, "function"))
+    stop("`fun` must be a function.", call. = FALSE)
+  if (!is.null(res) && !inherits(res, c("numeric", "integer")))
+    stop("`res` must be a numeric value.", call. = FALSE)
+  if (!is.null(raster_base) && !inherits(raster_base, "SpatRaster"))
+    stop("`raster_base` must be a SpatRaster object.", call. = FALSE)
+  if (!is.null(mask) && !inherits(mask, c("SpatRaster", "SpatVector")))
+    stop("`mask` must be a SpatRaster or SpatVector.", call. = FALSE)
   if (!inherits(verbose, "logical") || length(verbose) != 1)
-    stop("'verbose' must be a single logical value (TRUE or FALSE).", call. = FALSE)
+    stop("'verbose' must be a single logical value (TRUE or FALSE).",
+         call. = FALSE)
+  # crs
+  if (!inherits(crs, "character") || length(crs) != 1) {
+    stop("`crs` must be a single character string.", call. = FALSE)
+  }
+
 
   required_cols <- c(species, long, lat)
   if (!all(required_cols %in% names(occ))) {
     missing <- required_cols[!required_cols %in% names(occ)]
-    stop("The following columns are missing in `occ`: ", paste(missing, collapse = ", "), call. = FALSE)
+    stop("The following columns are missing in `occ`: ",
+         paste(missing, collapse = ", "), call. = FALSE)
   }
 
   occ <- occ[!is.na(occ[[long]]) & !is.na(occ[[lat]]), ]
 
-  target_crs <- "EPSG:4326"
-  if (!is.null(raster_base)) {
-    target_crs <- terra::crs(raster_base)
-  } else if (!is.null(mask)) {
-    target_crs <- terra::crs(mask)
-  }
-
   if (!is.null(raster_base)) {
     r_template <- raster_base
   } else {
-    if (is.null(res)) stop("You must provide either `raster_base` or a `res` value.", call. = FALSE)
+    if (is.null(res))
+      stop("You must provide either `raster_base` or a `res` value.",
+           call. = FALSE)
     ext_pts <- terra::ext(min(occ[[long]]), max(occ[[long]]),
                           min(occ[[lat]]), max(occ[[lat]]))
-    r_template <- terra::rast(ext_pts, res = res, crs = target_crs)
+    r_template <- terra::rast(ext_pts, res = res, crs = crs)
   }
 
-  pts <- terra::vect(occ, geom = c(long, lat), crs = "EPSG:4326")
-  if (terra::crs(pts) != target_crs) {
-    pts <- terra::project(pts, target_crs)
-  }
+  pts <- terra::vect(occ, geom = c(long, lat), crs = crs)
+
+  # if (terra::crs(pts) != target_crs) {
+  #   pts <- terra::project(pts, target_crs)
+  # }
 
   if (summary == "records") {
     r_final <- terra::rasterize(pts, r_template, fun = length)
@@ -119,14 +136,16 @@ richness_here <- function(occ, species = "species", long = "decimalLongitude",
       if (!is.null(names(field))) {
         spp_in_df <- unique(occ_internal[[species]])
         if (!all(spp_in_df %in% names(field))) {
-          if(verbose) warning("Some species in `occ` do not have a match in the `field` names. Values will be set to NA.")
+          if(verbose)
+            warning("Some species in `occ` do not have a match in the `field` names. Values will be set to NA.")
         }
         occ_internal$trait_val <- field[occ_internal[[species]]]
         trait_cols <- "trait_val"
       } else {
         if (!all(field %in% names(occ_internal))) {
           missing_f <- field[!field %in% names(occ_internal)]
-          stop("The following `field` columns are missing in `occ`: ", paste(missing_f, collapse = ", "), call. = FALSE)
+          stop("The following `field` columns are missing in `occ`: ",
+               paste(missing_f, collapse = ", "), call. = FALSE)
         }
         trait_cols <- field
       }
@@ -135,18 +154,23 @@ richness_here <- function(occ, species = "species", long = "decimalLongitude",
     occ_coords <- terra::geom(pts)[, c("x", "y")]
     cell_ids <- terra::cellFromXY(r_template, occ_coords)
 
-    temp_data <- data.frame(cell_id = cell_ids, species = occ_internal[[species]])
-    if (!is.null(trait_cols)) temp_data <- cbind(temp_data, occ_internal[, trait_cols, drop = FALSE])
+    temp_data <- data.frame(cell_id = cell_ids,
+                            species = occ_internal[[species]])
+    if (!is.null(trait_cols)) temp_data <- cbind(temp_data,
+                                                 occ_internal[, trait_cols,
+                                                              drop = FALSE])
 
     occ_unique <- unique(temp_data)
-    pts_unique <- terra::vect(terra::xyFromCell(r_template, occ_unique$cell_id), crs = target_crs)
+    pts_unique <- terra::vect(terra::xyFromCell(r_template,
+                                                occ_unique$cell_id), crs = crs)
 
     if (is.null(field)) {
       r_final <- terra::rasterize(pts_unique, r_template, fun = length)
       names(r_final) <- "richness"
     } else {
       for(col in trait_cols) pts_unique[[col]] <- occ_unique[[col]]
-      r_final <- terra::rasterize(pts_unique, r_template, field = trait_cols, fun = fun, na.rm = TRUE)
+      r_final <- terra::rasterize(pts_unique, r_template, field = trait_cols,
+                                  fun = fun, na.rm = TRUE)
       names(r_final) <- trait_cols
     }
   }
@@ -155,7 +179,7 @@ richness_here <- function(occ, species = "species", long = "decimalLongitude",
     if (terra::crs(mask) != terra::crs(r_final)) {
       mask <- terra::project(mask, terra::crs(r_final))
     }
-    r_final <- terra::mask(r_final, mask)
+    r_final <- terra::crop(r_final, mask, mask = TRUE)
   }
 
   return(r_final)
